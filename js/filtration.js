ee17b104ea54dc276845d85c7617fb1576ff5f4a
idaldu <dv.aleksander@yandex.ru>
2020-10-18 22:08:18 +0600

    Написал описание проекта, правки кода в JS

--- js/filtration.js
+++ js/filtration.js
@@ -1,12 +1,19 @@
 let select = document.querySelector('.select-theme');
 let articles = document.querySelectorAll('.articles');
+let articlesBig = document.querySelectorAll('.article-big');
 
 select.onchange = function () {
   for (let article of articles) {
     if (article.dataset.tag !== select.value && select.value !== 'all')  {
-      article.classList.add('hidden');
+      article.classList.add('hidden-block');
+    } else if (select.value === 'all') {
+        for (let articleBig of articlesBig) {
+          articleBig.style.marginTop = '62' + 'px';
+        };
+        article.classList.remove('hidden-block');
     } else {
-      article.classList.remove('hidden');
+        article.classList.remove('hidden-block');
+        article.style.marginTop = '0';
     }
   }
 }
